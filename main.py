8ca00eea2a7474cfe7246033d0581ecab79335a7
djumanov <djumanovdev@gmail.com>
2025-01-31 11:10:53 +0500

    query params

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
 @app.route("/blog")
 def blog_view():
-    return "Bu blog sahifa"
+    return "Blog"
 
 @app.route("/")
 def home_view():
-    return "Bu Home sahifa"
+    return "Home"
 
+@app.route("/add")
+def add():
+    args = request.args
 
-app.run()
+    a = int(args['a'])
+    b = int(args['b'])
+
+    result = a + b
+    print(result)
+
+    return f"{a} + {b} = {result}"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='localhost', port=8000)
